e43da2377654a674dd4043e747928a5df13b2dd4
Plexdi <senpai200798@gmail.com>
2025-12-01 12:46:54 +0000

    added method patch to cors allowmethod

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -43,7 +43,7 @@ func main() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"https://plexdistudio.com", "http://localhost:10000", "http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: false,
